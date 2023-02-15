a8b5e150c6c58fed011cf02a1a3e8827c2d98921
Pragati Sahu <pragatisahu1234@gmail.com>
2023-02-15 15:37:30 +0530

    Adding basename prop to configure router in the prod

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/admin">
       <Routes>
           <Route index element={<Home />} />
           <Route path="practice" element={<Exercise />} />
